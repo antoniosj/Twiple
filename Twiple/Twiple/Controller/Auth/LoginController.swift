10cf597f2d2c94a13b8765decd8e604ceb15358f
Antonio Silva <antonio.silvajr23@gmail.com>
2020-05-10 04:08:20 -0300

    WIP: Update login layout

--- Twiple/Twiple/Controller/Auth/LoginController.swift
+++ Twiple/Twiple/Controller/Auth/LoginController.swift
@@ -19,6 +19,41 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        //add email icon to the left of text
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "mail")
+        view.addSubview(iv)
+        
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        
+        iv.setDimensions(width: 24, height: 24)
+        
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemRed
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        //add email icon to the left of text
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        view.addSubview(iv)
+            
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+            
+        iv.setDimensions(width: 24, height: 24)
+            
+        
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -36,5 +71,13 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 0)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        //stackview
+        let stackView = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
     }
 }
